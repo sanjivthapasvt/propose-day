9898cb31e429a1b0db1d09b3865ba508b31bf5c4
Sanjiv Thapa <thapasvt12@gmail.com>
2026-02-07 07:58:47 +0545

    ✅ idk

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/proposeapp",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
